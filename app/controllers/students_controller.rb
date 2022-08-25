4016cb95a8c53321cc5e084f8f88980c196ed963
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-08-24 17:24:34 -0700

    Completed Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,37 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # students = Student.all
+    # render json: students
+
+    students = (params[:name] == nil) ?
+      Student.all
+      :
+      Student.all.filter{|s| s.last_name == params[:name].capitalize() || s.first_name == params[:name].capitalize()}
     render json: students
+  end
+
+  def grades
+    # model
+    grades = Student.all.order(grade: :desc)
+    #view
+    render json: grades
+  end
+
+  def highest_grade
+    # model
+    highest_grade = Student.all.order(grade: :desc).first
+    render json: highest_grade
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :grade)
   end
 
 end
